f71eb545f035c0c8811a9c4b239ff1c72ad29773
Bockjoo Kim <bockjoo@phys.ufl.edu>
2023-07-10 14:26:09 -0400

    Create transferBenchmarkTests.sh
    initial commit

---
+++ transferBenchmarkTests.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Change the configuration between # === and # ===
+# ========================================================
+notifytowhom=b__empty__oc__empty__kjoo__AT__gmail__dot__com
+workdir=$HOME/transferBenchmarkTests
+DST_DIR=$workdir
+export X509_USER_PROXY=_your_voms_proxy_file_
+
+#export PATH=/home/bockjoo/bin:/home/bockjoo/opt/cmsio2/gfal2/bin:/home/bockjoo/opt/cmsio2/anaconda3/bin:/home/bockjoo/opt/cmsio2/anaconda3/condabin:/apps/ufrc/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/opt/puppetlabs/bin:/opt/dell/srvadmin/bin:/home/bockjoo/services/external/webisoget-2.8.4/bin
+#export LD_LIBRARY_PATH=/home/bockjoo/opt/cmsio2/gfal2/lib64:/home/bockjoo/services/external/webisoget-2.8.4/lib:/usr/lib/x86_64-linux-gnu
+
+# ========================================================
+copytowhom=b__empty__oc__empty__kjoo__AT__gmail__dot__com
+regions="US T1 T2"
+ipvs="4 6"
+regions="US"
+regions="T1"
+regions="T2"
+regions="US T1 T2"
+ipvs="6"
+htmls=""
+#protocols="WebDAV"
+updown="Download"
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+[ -d $workdir ] || mkdir -p $workdir
+cd $workdir
+
+for r in $regions ; do
+   for ipv in $ipvs ; do
+    #for protocol in $protocols ; do
+     for updw in $updown ; do
+       echo python3 transferBenchmarkTests.py $r $ipv $updw >  transferBenchmarkTests_${r}_${ipv}_${updw}.log
+       python3 transferBenchmarkTests.py $r $ipv $updw >>  transferBenchmarkTests_${r}_${ipv}_${updw}.log
+       html=$(cat transferBenchmarkTests_${r}_${ipv}_${updw}.log | grep html | tail -1)
+       date_now=$(date +%Y%m%d%H%M%S)
+       dst_html=${DST_DIR}/$(basename $html | sed "s#\.html#${date_now}.html#")
+       echo INFO copying $html to $dst_html
+       /bin/cp $html $dst_html
+       htmls="$htmls $dst_html"
+     done
+    #done
+   done
+done
+(
+     echo "To: "$(echo $notifytowhom | sed "s#__AT__#@#" | sed "s#__dot__#\.#g" | sed "s#__empty__##g")
+     echo "Cc: "$(echo $notifytowhom | sed "s#__AT__#@#" | sed "s#__dot__#\.#g" | sed "s#__empty__##g")
+     echo "Subject: transferBenchmarkTests on $(/bin/hostname -s)"
+     echo "Content-Type: text/html"
+     echo
+     for html in $htmls ; do
+	 cat $html
+     done
+     echo
+) | /usr/sbin/sendmail -t
+cd -
+exit 0
